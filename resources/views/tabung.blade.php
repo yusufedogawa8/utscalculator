9091dfce982fdabe2649159cb4c38c3a4830e826
yusufedogawa8 <yusufedogawa8@gmail.com>
2020-09-15 13:53:00 +0700

    utsCalculator

--- resources/views/tabung.blade.php
+++ resources/views/tabung.blade.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/84686cd467.js" crossorigin="anonymous"></script>
     <title>Menghitung Tabung</title>
 </head>
 <body>
 <!-- NAVBAR -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand text-uppercase font-weight-bold">CALCULATE SITE</a>
+        <a class="navbar-brand text-uppercase font-weight-bold"><i class="fas fa-calculator"></i> CALCULATOR SITE</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
