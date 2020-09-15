a0095a70111e201daf5ea7904fdc23511ce7b694
yusufedogawa8 <yusufedogawa8@gmail.com>
2020-09-15 13:35:38 +0700

    utsCalculator

--- resources/views/bola.blade.php
+++ resources/views/bola.blade.php
@@ -4,12 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
-    <title>Calculator</title>
-    <style>
-        .luas {
-            padding: 12px;
-        }
-    </style>
+    <title>Menghitung Bola</title>
 </head>
 <body>
 <!-- NAVBAR -->
